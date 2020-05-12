d7b8bb9158aaa8a2ec105fd42444c006cba42a5b
隈本優希 <morikuma1019@kumamotoyuukinoMacBook-Air.local>
2020-05-12 19:23:12 +0900

    fix(github-actions): fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBfqvFe1TbpgYw4G3iTpLW5Gvus7JQnmDQ',
+    authDomain: 'nasitoge-2dc56.firebaseapp.com',
+    databaseURL: 'https://nasitoge-2dc56.firebaseio.com',
+    projectId: 'nasitoge-2dc56',
+    storageBucket: 'nasitoge-2dc56.appspot.com',
+    messagingSenderId: '732299216473',
+    appId: '1:732299216473:web:7da6f9ad231b0efb3fd348',
+    measurementId: 'G-QPTPWZSTSG',
+  }
 };
