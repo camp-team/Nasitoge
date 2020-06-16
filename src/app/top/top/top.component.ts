f080a786a71df83b2eb014ebab3f4d6b5e8449d1
隈本優希 <morikuma1019@kumamotoyuukinoMacBook-Air.local>
2020-06-16 17:44:56 +0900

    Top画面作成

--- src/app/top/top/top.component.ts
+++ src/app/top/top/top.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-top',
   templateUrl: './top.component.html',
-  styleUrls: ['./top.component.scss']
+  styleUrls: ['./top.component.scss'],
 })
 export class TopComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  login() {
+    this.authService.login();
   }
 
+  ngOnInit(): void {}
 }
