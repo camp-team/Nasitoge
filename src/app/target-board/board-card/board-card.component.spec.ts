20298a9448dcb6f8db8bed4f03a1304a10fff81d
隈本優希 <morikuma1019@kumamotoyuukinoMacBook-Air.local>
2020-09-17 18:50:31 +0900

    feat(board-card, board-list): コンポーネント作成

---
+++ src/app/target-board/board-card/board-card.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BoardCardComponent } from './board-card.component';
+
+describe('BoardCardComponent', () => {
+  let component: BoardCardComponent;
+  let fixture: ComponentFixture<BoardCardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [BoardCardComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BoardCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
